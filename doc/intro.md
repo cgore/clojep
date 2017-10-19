df6959e6ad23b925c836b2bb1cbb0b3da3735c52
Christopher Mark Gore <cgore@cgore.com>
2017-10-19 09:56:49 -0500

    initial commit from `lein init clojep`

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to clojep
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/clojep/core_test.clj
@@ -0,0 +1,7 @@
+(ns clojep.core-test
+  (:require [clojure.test :refer :all]
+            [clojep.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
